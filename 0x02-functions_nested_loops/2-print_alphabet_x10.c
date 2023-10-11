baca8f5e4af74153b013be078e258f4b3d34126c
E-LyonDev <eelyondesign@gmail.com>
2023-10-11 07:47:30 -0700

    update alphabet 10 time lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Make alphabet x10 times.
+ * print_alphabet_x10 - Make alphabet x10 times
  * Return: void
  */
 
@@ -11,13 +11,14 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
-	while (i < 10)
+	i = 0;
+	while  (i < 10)
 	{
-		for (ch = 'a'; ch <= 'z'; ch++)
-		{
-			_putchar(ch);
-		}
-		_putchar('\n');
-		i++;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	_putchar(ch);
+	}
+	_putchar('\n');
+	i++;
 	}
 }
