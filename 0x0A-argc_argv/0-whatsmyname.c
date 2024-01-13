122df56b8448060c6f24086dc636674ae4885825
E-LyonDev <eelyondesign@gmail.com>
2024-01-12 21:32:37 -0800

    Still on correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,10 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
  * main - A function that prints its name
- *
  * @argc: argument count
  * @argv: argument vector
- *
- * Return: Always 0 (success)
+ * Return: (0) when successful
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
