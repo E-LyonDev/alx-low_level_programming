5d5bfd5f46dccbca25a63ad0b1c24beae0848f1c
E-LyonDev <eelyondesign@gmail.com>
2024-01-13 02:24:40 -0800

    A program thaat prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,8 @@
  * Return: 0 when successful
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
